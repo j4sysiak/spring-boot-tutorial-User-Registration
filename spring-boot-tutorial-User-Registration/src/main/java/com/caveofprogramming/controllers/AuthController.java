d86f86f1bfc1bf8c38a675cf13266475c3addd4f
j4sysiak <j4sysiak@gmail.com>
2018-12-16 18:35:50 +0100

    1111

---
+++ spring-boot-tutorial-User-Registration/src/main/java/com/caveofprogramming/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.caveofprogramming.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+
+//	@RequestMapping("/admin")
+//	String admin() {
+//		return "admin";
+//	}
+	
+	@RequestMapping("/login")
+	String login() {
+		return "app.login";
+	}
+}
